fc750a4d6537031ef01d3bfb2f98bc0ef5cd930c
acrylic-style <me@acrylicstyle.xyz>
2020-07-27 06:21:09 +0900

    gui!!!!!

--- src/main/java/xyz/acrylicstyle/backup/AutoBackupPlayerData.java
+++ src/main/java/xyz/acrylicstyle/backup/AutoBackupPlayerData.java
@@ -2,6 +2,7 @@ package xyz.acrylicstyle.backup;
 
 import org.apache.commons.io.FileUtils;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -9,15 +10,21 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import util.CollectionList;
 import util.ICollectionList;
+import xyz.acrylicstyle.shared.BaseMojangAPI;
+import xyz.acrylicstyle.tomeito_api.TomeitoAPI;
+import xyz.acrylicstyle.tomeito_api.command.PlayerCommandExecutor;
 import xyz.acrylicstyle.tomeito_api.events.player.PlayerPreDeathEvent;
+import xyz.acrylicstyle.tomeito_api.gui.PerPlayerInventory;
 import xyz.acrylicstyle.tomeito_api.providers.ConfigProvider;
 import xyz.acrylicstyle.tomeito_api.utils.Log;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Comparator;
 import java.util.Date;
+import java.util.Objects;
 import java.util.UUID;
 import java.util.function.Consumer;
 
@@ -25,13 +32,44 @@ public class AutoBackupPlayerData extends JavaPlugin implements Listener {
     public static ConfigProvider config = null;
     public static int period = 10; // minutes
     public static int keepFiles = 100;
+    public static AutoBackupPlayerData instance = null;
+
+    public static String timestampToDate(long timestamp) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(timestamp);
+        String m = Integer.toString(calendar.get(Calendar.MINUTE));
+        String s = Integer.toString(calendar.get(Calendar.SECOND));
+        return calendar.get(Calendar.YEAR) + "/"
+                + (calendar.get(Calendar.MONTH) + 1) + "/"
+                + calendar.get(Calendar.DAY_OF_MONTH) + " "
+                + calendar.get(Calendar.HOUR_OF_DAY) + ":"
+                + (m.length() == 1 ? "0" + m : m) + ":"
+                + (s.length() == 1 ? "0" + s : s);
+    }
+
+    public static final PerPlayerInventory<PlayerDataRestoreGui> gui = new PerPlayerInventory<>(PlayerDataRestoreGui::new);
 
     @Override
     public void onEnable() {
+        instance = this;
         config = new ConfigProvider("./plugins/AutoBackupPlayerData/config.yml");
         period = config.getInt("delayMinute", 10);
         keepFiles = config.getInt("keepFiles", 100);
         Bukkit.getPluginManager().registerEvents(this, this);
+        TomeitoAPI.registerCommand("playerdata", new PlayerCommandExecutor() {
+            @Override
+            public void onCommand(Player player, String[] args) {
+                UUID uuid;
+                try {
+                    uuid = BaseMojangAPI.getUniqueId(args[0]);
+                    if (uuid == null) throw new NullPointerException();
+                } catch (RuntimeException ex) {
+                    player.sendMessage(ChatColor.RED + "プレイヤーが見つかりません。");
+                    return;
+                }
+                player.openInventory(gui.get(uuid).getInventory());
+            }
+        });
         new BukkitRunnable() {
             @Override
             public void run() {
@@ -70,8 +108,7 @@ public class AutoBackupPlayerData extends JavaPlugin implements Listener {
             dest.delete();
             try {
                 FileUtils.copyFile(src, dest);
-            } catch (IOException ignore) {
-            }
+            } catch (IOException ignore) {}
             Log.info("Saved data for " + e.getPlayer().getName());
         }).start();
     }
@@ -79,6 +116,7 @@ public class AutoBackupPlayerData extends JavaPlugin implements Listener {
     @SuppressWarnings("ResultOfMethodCallIgnored")
     private Consumer<UUID> saveConsumer(long time) {
         return uuid -> {
+            if (Bukkit.getPlayer(uuid) != null) Objects.requireNonNull(Bukkit.getPlayer(uuid)).saveData();
             File src = new File("./world/playerdata/" + uuid.toString() + ".dat");
             File dest = new File("./backupplayerdata/" + time + "/" + uuid.toString() + ".dat");
             dest.mkdirs();
---
+++ src/main/java/xyz/acrylicstyle/backup/PlayerDataRestoreGui.java
@@ -0,0 +1,189 @@
+package xyz.acrylicstyle.backup;
+
+import org.apache.commons.io.FileUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.jetbrains.annotations.NotNull;
+import util.Collection;
+import util.CollectionList;
+import util.ICollectionList;
+import xyz.acrylicstyle.api.MojangAPI;
+import xyz.acrylicstyle.tomeito_api.sounds.Sound;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Comparator;
+import java.util.Objects;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class PlayerDataRestoreGui implements xyz.acrylicstyle.tomeito_api.gui.PlayerGui, InventoryHolder, Listener {
+    private final UUID uuid;
+    private static final ItemStack bigBlack;
+    private final CollectionList<Inventory> inventories = new CollectionList<>();
+    private final Collection<Integer, Collection<Integer, File>> fi = new Collection<>();
+
+    static {
+        bigBlack = new ItemStack(Material.BLACK_STAINED_GLASS_PANE);
+        ItemMeta glassMeta = bigBlack.getItemMeta();
+        assert glassMeta != null;
+        glassMeta.setDisplayName(" ");
+        bigBlack.setItemMeta(glassMeta);
+    }
+
+    public PlayerDataRestoreGui(UUID uuid) {
+        Bukkit.getPluginManager().registerEvents(this, AutoBackupPlayerData.instance);
+        this.uuid = uuid;
+        buildGui();
+    }
+
+    private void buildGui() {
+        inventories.clear();
+        fi.clear();
+        AtomicInteger page = new AtomicInteger();
+        AtomicInteger index = new AtomicInteger();
+        File folder = new File("./backupplayerdata");
+        AtomicReference<Inventory> inventory = new AtomicReference<>(setItems(Bukkit.createInventory(this, 54, "プレイヤーデータ復元 - Page " + page.incrementAndGet())));
+        inventory.get().setItem(45, bigBlack);
+        if (folder.listFiles() != null) {
+            CollectionList<File> files = ICollectionList.asList(folder.listFiles());
+            files.sort(Comparator.comparingLong(File::lastModified));
+            files.sort(Comparator.reverseOrder());
+            files.forEach(file -> {
+                if (index.get() > 44) {
+                    inventories.add(inventory.get());
+                    inventory.set(setItems(Bukkit.createInventory(this, 54, "プレイヤーデータ復元 - Page " + page.incrementAndGet())));
+                    index.set(0);
+                }
+                File f = new File(file.getAbsolutePath() + "/");
+                if (new File(f.getAbsolutePath() + "/" + uuid.toString() + ".dat").exists()) {
+                    if (!fi.containsKey(page.get())) fi.add(page.get(), new Collection<>());
+                    fi.get(page.get()).add(index.get(), f);
+                    inventory.get().setItem(index.getAndIncrement(), getItemStack(file));
+                }
+            });
+        }
+        inventory.get().setItem(53, bigBlack);
+        inventories.add(inventory.get());
+    }
+
+    private ItemStack getItemStack(File file) {
+        ItemStack item = new ItemStack(Material.CAKE);
+        ItemMeta meta = item.getItemMeta();
+        meta.setDisplayName(ChatColor.LIGHT_PURPLE + AutoBackupPlayerData.timestampToDate(file.lastModified()));
+        item.setItemMeta(meta);
+        return item;
+    }
+
+    private Inventory setItems(Inventory inventory) {
+        inventory.setItem(45, arrowItem("← 戻る"));
+        inventory.setItem(46, bigBlack);
+        inventory.setItem(47, bigBlack);
+        inventory.setItem(48, bigBlack);
+        inventory.setItem(49, bigBlack);
+        inventory.setItem(50, bigBlack);
+        inventory.setItem(51, bigBlack);
+        inventory.setItem(52, bigBlack);
+        inventory.setItem(53, arrowItem("次へ →"));
+        return inventory;
+    }
+
+    private ItemStack arrowItem(String name) {
+        ItemStack arrow = new ItemStack(Material.ARROW);
+        ItemMeta meta = arrow.getItemMeta();
+        assert meta != null;
+        meta.setDisplayName(name);
+        arrow.setItemMeta(meta);
+        return arrow;
+    }
+
+    @Override
+    public @NotNull Inventory getInventory() {
+        return Objects.requireNonNull(inventories.first());
+    }
+
+    private int page = 1;
+    private boolean preventLoop = false;
+
+    @EventHandler
+    public void onInventoryOpen(InventoryOpenEvent e) {
+        if (e.getInventory().getHolder() != this) return;
+        if (preventLoop) return;
+        preventLoop = true;
+        e.getPlayer().closeInventory();
+        buildGui();
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                e.getPlayer().openInventory(getInventory());
+                preventLoop = false;
+            }
+        }.runTaskLater(AutoBackupPlayerData.instance, 1);
+    }
+
+    @Override
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent e) {
+        if (e.getInventory().getHolder() != this) return;
+        if (e.getClick() == ClickType.SHIFT_LEFT || e.getClick() == ClickType.SHIFT_RIGHT) {
+            e.setCancelled(true);
+            return;
+        }
+        if (e.getClickedInventory() == null || e.getClickedInventory().getHolder() != this) return;
+        e.setCancelled(true);
+        if (e.getSlot() == 45) {
+            if (page-1 < 0) {
+                e.setCancelled(true);
+                return;
+            }
+            e.getWhoClicked().openInventory(inventories.get(--page));
+        } else if (e.getSlot() == 53) {
+            if (page+1 >= inventories.size()) {
+                e.setCancelled(true);
+                return;
+            }
+            e.getWhoClicked().openInventory(inventories.get(++page));
+        }
+        if (e.getSlot() >= 45) return;
+        File f = fi.get(page).get(e.getSlot());
+        File dest = new File("./world/playerdata/" + uuid.toString() + ".dat");
+        try {
+            FileUtils.copyFile(new File(f.getAbsolutePath() + "/" + uuid.toString() + ".dat"), dest);
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+        Player player = Bukkit.getPlayer(uuid);
+        if (player != null) player.loadData();
+        e.getWhoClicked().sendMessage(ChatColor.GREEN + MojangAPI.getName(uuid) + "のデータを復元しました。");
+        e.getWhoClicked().sendMessage(ChatColor.GREEN + "復元したバックアップ: " + AutoBackupPlayerData.timestampToDate(f.lastModified()));
+        ((Player) e.getWhoClicked()).playSound(e.getWhoClicked().getLocation(), Sound.BLOCK_NOTE_PLING, 100F, 2F);
+    }
+
+    @Override
+    @EventHandler
+    public void onInventoryDrag(InventoryDragEvent e) {
+        if (e.getInventory().getHolder() != this) return;
+        e.setCancelled(true);
+    }
+
+    @Override
+    @EventHandler
+    public void onInventoryClose(InventoryCloseEvent e) {
+        page = 1;
+    }
+}
